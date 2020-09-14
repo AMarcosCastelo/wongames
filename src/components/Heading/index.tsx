bbcdb4d343766e492a134a305ae68ed9d47a0a61
marcos-12marcos@hotmail.com <marcos-12marcos@hotmail.com>
2020-09-14 12:01:58 -0300

    update heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import * as S from './styled';
 
+export type LineColors = 'primary' | 'secondary';
+
 export type HeadingProps = {
   children: React.ReactNode;
   color?: 'white' | 'black';
   lineLeft?: boolean;
   lineBottom?: boolean;
+  lineColor?: LineColors;
+  size?: 'small' | 'medium';
 };
 
 const Heading = ({
   children,
   color = 'black',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium',
+  lineColor = 'primary'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    size={size}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+  >
     {children}
   </S.Wrapper>
 );
